7f780911c0abef65db811d2473eba8776e8bc709
EVGEN <mtmsys4@gmail.com>
2022-09-03 03:55:51 +0300

    Домашнее задание выполнено

--- Program.cs
+++ Program.cs
@@ -1,61 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-/*
- черновик
-
-int[] Generator (int minValue, int maxValue, int size )
-{
-   
-    int[] arriva = new int [size];
-    for (int i = 0; i < size; i++ )
-    {
-        arriva[i] = new Random().Next(minValue, maxValue+1);
-    }
-   
-     return arriva;
-}
-
-int [] NewArrakis (int[] array)
-
-{
-    int [] arrakis = new int [array.Length];
-    int count = array.Length-1;
-    for (int h = 0; h < array.Length; h++)
-    {
-        if (count != h)
-        {
-        arrakis[h] = array[h]*array[count]  ;
-        }
-        else
-        {
-            arrakis[h] = array[h];
-        }
-        count--;
-    }
-
-    return arrakis;
-}
-
-void Chisl (int[] arriva)
-{
-    Console.Write("[ ");
-    for (int f = 0; f < arriva.Length; f++)
-     {
-            Console.Write(arriva[f]);
-            Console.Write(", ");
-     }
-     Console.Write("\b\b ");
-     Console.Write(" ]");
-}
-
-*/
-
+﻿
+//__________________________Домашняя работа семинар 5_________________________
 
 //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 // Напишите программу, которая покажет количество чётных чисел в массиве.
-
-//[345, 897, 568, 234] -> 2
 
 /* Решение внутри
 
@@ -110,20 +57,162 @@ int [] sammas = Generator(100, 1000, razmer);
 
 int kolvo = summaPol(sammas);
 
-Console.Write(" Сумма четных чисел в массиве: ");
+Console.Write(" Количество четных чисел в массиве: ");
 Chisl(sammas);
-Console.Write($" равна: {kolvo}");
+Console.Write($" равно: {kolvo}");
 Console.WriteLine(" ");
 
 */
 
+//__________________________________________________
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-//[3, 7, 23, 12] -> 19
+/* Решение внутри
 
-//[-4, -6, 89, 6] -> 0
+int[] Generator (int minValue, int maxValue, int size )
+{
+   
+    int[] arriva = new int [size];
+    for (int i = 0; i < size; i++ )
+    {
+        arriva[i] = new Random().Next(minValue, maxValue+1);
+    }
+   
+     return arriva;
+}
+
+int zumma (int[] array)
+
+{
+    int zum = 0;
+
+    for (int h = 0; h < array.Length; h++)
+    {
+      
+        int ostatok = h % 2;
+
+        if (ostatok > 0)
+        {
+        zum = zum + array[h] ;
+        }
+    }
+
+    return zum;
+}
+
+void Chisl (int[] arriva)
+{
+    Console.Write("[ ");
+    for (int f = 0; f < arriva.Length; f++)
+     {
+            Console.Write(arriva[f]);
+            Console.Write(", ");
+     }
+     Console.Write("\b\b ");
+     Console.Write(" ]");
+}
+
+
+Console.Write("Пожалуйста, введите размер массива: ");
+
+int razm = Convert.ToInt32(Console.ReadLine());
+
+int[] newMassiv = Generator(-99, 99, razm);
+
+int summ = zumma(newMassiv);
+
+
+Console.Write(" Сумма элементов, стоящих на нечетных позициях в массиве: ");
+Chisl(newMassiv);
+Console.Write($" Равна: {summ} ");
+Console.WriteLine("Конец программы.");
+
+*/
+
+//____________________________________________________________________
 
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-//[3 7 22 2 78] -> 76
+/* Решение внутри
+
+double[] Generator (int minValue, int maxValue, int size )
+{
+   
+    double[] arriva = new double [size];
+    for (int i = 0; i < size; i++ )
+    {
+        Random rand = new Random();
+
+        double dobav = rand.NextDouble(); // число от 0 до 1
+
+        arriva[i] = (new Random().Next(minValue, maxValue+1)) + dobav;
+    }
+   
+     return arriva;
+}
+
+double minElem (double[] array)
+
+{
+    double minZum = array[0];
+
+    for (int h = 0; h < array.Length; h++)
+    {
+      
+        if (array[h] < minZum)
+        {
+        minZum = array[h] ;
+        }
+    }
+
+    return minZum;
+}
+
+double maxElem (double[] array)
+
+{
+    double maxZum = array[0];
+
+    for (int h = 0; h < array.Length; h++)
+    {
+      
+        if (array[h] > maxZum)
+        {
+        maxZum = array[h] ;
+        }
+    }
+
+    return maxZum;
+}
+
+void Chisl (double[] arriva)
+{
+    Console.Write("[ ");
+    for (int f = 0; f < arriva.Length; f++)
+     {
+            Console.Write(arriva[f]);
+            Console.Write(", ");
+     }
+     Console.Write("\b\b ");
+     Console.Write("]");
+}
+
+Console.Write("Пожалуйста введите размер массива: ");
+int razzm = Convert.ToInt32(Console.ReadLine());
+
+double [] massivVesh = Generator(-99, 99, razzm);
+
+double minNum =  minElem(massivVesh);
+
+double maxNum = maxElem(massivVesh);
+
+double raznica = maxNum - minNum;
+
+Console.WriteLine($" Разница между максимальным и минимальным элементом в массиве: ");
+Chisl(massivVesh);
+Console.Write($" равна: {raznica} ");
+Console.WriteLine(" ");
+Console.WriteLine("Конец программы.");
+
+*/
